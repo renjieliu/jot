819fba04279f31bf542175276ff0aab481ec5244
Renjie Liu <anlrj@qq.com>
2025-03-11 21:55:45 -0400

    。

---
+++ prompt.py
@@ -0,0 +1,16 @@
+def insideFunc():
+    return "Hello! this is from the inside function"
+
+def outsideFunc():
+    pic = "To generate a picture with all the basketball in the sky"
+    return pic
+
+
+def main():
+    outsideFunc()
+
+
+print("please enter something, so I can help")
+main()
+
+
